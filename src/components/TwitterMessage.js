655f43a212e1f0644eb3a413b480c4c7ac043af4
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-18 08:05:25 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -10,6 +10,20 @@ class TwitterMessage extends React.Component {
     };
   }
 
+ handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    if (!this.state.username || !this.state.password) return
+
+    this.props.handleLogin(this.state)
+  }
+
   render() {
     return (
       <div>
